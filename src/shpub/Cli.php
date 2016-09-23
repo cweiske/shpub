956202bfa5b27a89f5c435640590188524889225
Christian Weiske <cweiske@cweiske.de>
2016-09-23 13:46:41 +0200

    Release 0.3.0
    Signed-off-by: Christian Weiske <cweiske@cweiske.de>

--- src/shpub/Cli.php
+++ src/shpub/Cli.php
@@ -91,7 +91,7 @@ class Cli
         $optParser = new \Console_CommandLine();
         $optParser->name        = 'shpub';
         $optParser->description = 'Command line micropub client';
-        $optParser->version     = '0.2.1';
+        $optParser->version     = '0.3.0';
         $optParser->subcommand_required = true;
 
         $optParser->addOption(
