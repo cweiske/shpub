73b851213bad76c93ec28511b39b37bc12039961
Christian Weiske <cweiske@cweiske.de>
2017-10-28 15:15:57 +0200

    Prepare 0.5.2

--- src/shpub/Cli.php
+++ src/shpub/Cli.php
@@ -95,7 +95,7 @@ class Cli
         $optParser = new \Console_CommandLine();
         $optParser->name        = 'shpub';
         $optParser->description = 'Command line micropub client';
-        $optParser->version     = '0.5.1';
+        $optParser->version     = '0.5.2';
         $optParser->subcommand_required = true;
 
         $optParser->addOption(
